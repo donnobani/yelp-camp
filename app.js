0ee416a9f2e0df9ffefa93de4b573305d6cbf0cd
donnobani <55165390+donnobani@users.noreply.github.com>
2022-10-06 20:52:25 -0400

    created index page and route
    created basic index page and it's routing using express

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const mongoose = require("mongoose");
 
 const Campground = require("./models/campground");
 
+// launches connection to db
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
+// checks for connection errors
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 });
 
+// allows ejs to work through render
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -19,12 +22,13 @@ app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
 
-app.get("/", (req, res) => {
-  res.render("home");
+// index route (all campgrounds)
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({ title: "My backyard" });
-  await camp.save();
-  res.send(camp);
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", campground);
 });
