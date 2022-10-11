ce5a1077a120ac1956de21fb0bc29dfbcace1b2b
donnobani <55165390+donnobani@users.noreply.github.com>
2022-10-10 21:20:52 -0400

    created delete functionality
    finished CRUD capabilities

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methorOverride = require("method-override");
 
 const Campground = require("./models/campground");
 const { urlencoded } = require("express");
@@ -20,6 +21,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 // used to parse request body into json
 app.use(express.urlencoded({ extended: true }));
+// allows us to use put requuest from form
+app.use(methorOverride("_method"));
 
 app.listen(3000, () => {
   console.log("Serving on port 3000");
@@ -48,7 +51,28 @@ app.get("/campgrounds/:id", async (req, res) => {
 
 // creates new campground and redirects to new campground
 app.post("/campgrounds", async (req, res) => {
-  const campground = new Campground(req.body.campground);
+  const campground = new Campground(req.body.campground); // grouping allowed by using campground[property] as name
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// serves form to edit campground
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+// updates campground and redirects to show page
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground, // spread operator
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
 });
