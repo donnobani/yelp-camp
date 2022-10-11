3dcab03dfcdc0a23f2d7723982bc7b3ea38dd8d0
donnobani <55165390+donnobani@users.noreply.github.com>
2022-10-10 20:44:29 -0400

    created new and create routes
    created route to serve form for new campground and route to post new campground

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 
 const Campground = require("./models/campground");
+const { urlencoded } = require("express");
 
 // launches connection to db
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
@@ -17,9 +18,15 @@ db.once("open", () => {
 // allows ejs to work through render
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+// used to parse request body into json
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("Serving on port 3000");
+});
+
+app.get("/", async (req, res) => {
+  res.render("home");
 });
 
 // index route (all campgrounds)
@@ -28,8 +35,20 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
+// serves form for new campground
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
 // show route (details for one campground)
 app.get("/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
+});
+
+// creates new campground and redirects to new campground
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
