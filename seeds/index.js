cd25dadfa5772beff6d884090545c7677bff1ee1
donnobani <55165390+donnobani@users.noreply.github.com>
2022-10-15 16:41:03 -0400

    added styling to views and pictures to model
    added styling via bootstrap and added image and description tokens for the camoground model

--- seeds/index.js
+++ seeds/index.js
@@ -17,14 +17,22 @@ db.once("open", () => {
 // pass in an array, returns random element from that array
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+// sample description
+const description =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus commodi repudiandae perferendis molestiae autem minima ut corrupti nihil, magni eius et odit exercitationem repellat! Reiciendis debitis illum sed repellat ex!";
+
 // populates collection
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const rand = Math.floor(Math.random() * 1000); // inside portion gets us random number, floor gets us an int
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[rand].city}, ${cities[rand].state}`,
       title: `${sample(descriptors)} ${sample(places)} `,
+      image: `https://source.unsplash.com/random/300x300?camping,${i}`,
+      description,
+      price,
     });
     await camp.save();
   }
